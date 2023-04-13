c92bdc4231bbabb9f14412bb85d1ecbafd750e1c
Sonya <vilinskaya.s@bk.ru>
2023-04-13 11:31:02 +0300

    Добавили решения задач 64,66,68

---
+++ task66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу, которая
+// найдёт сумму натуральных элементов в промежутке от M до N.
+// Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int res = SumNumbers(m, n);
+Console.WriteLine(res);
+
+int SumNumbers(int x1, int x2)
+{
+    if (x1 == x2) return x2;
+    return x1 + SumNumbers(x1+1, x2);
+}
+
---
+++ task68/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу вычисления функции Аккермана 
+//с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int res = SumAcc(m, n);
+Console.WriteLine(res);
+
+int SumAcc(int x1, int x2)
+{
+    if (x1 == 0) return x2+1;
+    if (x1 > 0 && x2 == 0)
+        return SumAcc(x1-1,1);
+    else
+        return SumAcc(x1-1,SumAcc(x1, x2-1));
+}
